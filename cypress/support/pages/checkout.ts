5a61a7a31bc87710b3117d70f659d0e5eddb8a27
djonymalta <djonymalta@Mac-mini-de-Djony.local>
2023-04-20 09:47:06 -0300

    Realizado remoção de espaços em branco e identação do código

--- cypress/support/pages/checkout.ts
+++ cypress/support/pages/checkout.ts
@@ -2,25 +2,26 @@
 
 import { FORMULARIO } from "./elements";
 
-class dados{
-    checkout(){
-        cy.wait(5000)
-        cy.get(FORMULARIO.email).type(Cypress.env('dados').email)
-        cy.get(FORMULARIO.primeiroNome).type(Cypress.env('dados').primeiroNome)
-        cy.get(FORMULARIO.ultimoNome).type(Cypress.env('dados').ultimoNome)
-        cy.get(FORMULARIO.companhia).type(Cypress.env('dados').companhia)
-        cy.get(FORMULARIO.enderecoCampo1).type(Cypress.env('dados').enderecoCampo1)
-        cy.get(FORMULARIO.enderecoCampo2).type(Cypress.env('dados').enderecoCampo2)
-        cy.get(FORMULARIO.enderecoCampo3).type(Cypress.env('dados').enderecoCampo3)
-        cy.get(FORMULARIO.cidade).type(Cypress.env('dados').cidade)
-        cy.get(FORMULARIO.estado).select(Cypress.env('dados').estado)
-        cy.get(FORMULARIO.cep).type(Cypress.env('dados').cep)
-        cy.wait(3000)
-        cy.get(FORMULARIO.telefone).type(Cypress.env('dados').telefone)
-        cy.get(FORMULARIO.frete).click()
-        cy.get('.button').click({force:true})
-        cy.get('.payment-method-content > :nth-child(4) > div.primary > .action').click()
-    }
+class dados {
+  checkout() {
+    cy.wait(5000);
+    cy.get(FORMULARIO.email).type(Cypress.env("dados").email);
+    cy.get(FORMULARIO.primeiroNome).type(Cypress.env("dados").primeiroNome);
+    cy.get(FORMULARIO.ultimoNome).type(Cypress.env("dados").ultimoNome);
+    cy.get(FORMULARIO.companhia).type(Cypress.env("dados").companhia);
+    cy.get(FORMULARIO.enderecoCampo1).type(Cypress.env("dados").enderecoCampo1);
+    cy.get(FORMULARIO.enderecoCampo2).type(Cypress.env("dados").enderecoCampo2);
+    cy.get(FORMULARIO.enderecoCampo3).type(Cypress.env("dados").enderecoCampo3);
+    cy.get(FORMULARIO.cidade).type(Cypress.env("dados").cidade);
+    cy.get(FORMULARIO.estado).select(Cypress.env("dados").estado);
+    cy.get(FORMULARIO.cep).type(Cypress.env("dados").cep);
+    cy.wait(3000);
+    cy.get(FORMULARIO.telefone).type(Cypress.env("dados").telefone);
+    cy.get(FORMULARIO.frete).click();
+    cy.get(".button").click({ force: true });
+    cy.get(
+      ".payment-method-content > :nth-child(4) > div.primary > .action"
+    ).click();
+  }
 }
-
 export default new dados();
--- cypress/support/pages/elements.ts
+++ cypress/support/pages/elements.ts
@@ -1,30 +1,27 @@
+const PRODUTO = {
+  tamnhoxs: "#option-label-size-143-item-166",
+  corlaranja: "#option-label-color-93-item-56",
+  botaoaddtocart:
+    "#maincontent > div.columns > div > div.widget.block.block-static-block > div.block.widget.block-products-list.grid > div > div > ol > li:nth-child(1) > div > div > div.product-item-inner > div > div.actions-primary > form > button",
+};
 
-const PRODUTO =  {
-    tamnhoxs: '#option-label-size-143-item-166',
-    corlaranja: '#option-label-color-93-item-56',
-    botaoaddtocart: '#maincontent > div.columns > div > div.widget.block.block-static-block > div.block.widget.block-products-list.grid > div > div > ol > li:nth-child(1) > div > div > div.product-item-inner > div > div.actions-primary > form > button',
-
-}
-
- const CARRINHO = {
-    botaocarrinho: '.showcart',
-    fazercheckout: 'button[id="top-cart-btn-checkout"]'
-}
+const CARRINHO = {
+  botaocarrinho: ".showcart",
+  fazercheckout: 'button[id="top-cart-btn-checkout"]',
+};
 
 const FORMULARIO = {
-    email: '#customer-email',
-    primeiroNome: 'input[name="firstname"',
-    ultimoNome: 'input[name="lastname"',
-    companhia:'input[name="company"',
-    enderecoCampo1: 'input[name="street[0]"',
-    enderecoCampo2: 'input[name="street[1]"',
-    enderecoCampo3: 'input[name="street[2]"',
-    cidade: 'input[name="city"',
-    estado: '.control  .select[name="region_id"',
-    cep: 'input[name="postcode"',
-    telefone: 'input[name="telephone"]',
-    frete: ':nth-child(1) > :nth-child(1) > .radio'
-
-}
-
-export {PRODUTO, CARRINHO, FORMULARIO}
+  email: "#customer-email",
+  primeiroNome: 'input[name="firstname"',
+  ultimoNome: 'input[name="lastname"',
+  companhia: 'input[name="company"',
+  enderecoCampo1: 'input[name="street[0]"',
+  enderecoCampo2: 'input[name="street[1]"',
+  enderecoCampo3: 'input[name="street[2]"',
+  cidade: 'input[name="city"',
+  estado: '.control  .select[name="region_id"',
+  cep: 'input[name="postcode"',
+  telefone: 'input[name="telephone"]',
+  frete: ":nth-child(1) > :nth-child(1) > .radio",
+};
+export { PRODUTO, CARRINHO, FORMULARIO };
