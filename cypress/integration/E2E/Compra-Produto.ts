15d2a207c7c492fb067faeddd354a6258c85eb2d
djonymalta <djonymalta@Mac-mini-de-Djony.local>
2023-04-20 16:56:52 -0300

    Realizado remoção de cy.wait e realizado modificações de timeout

--- cypress/integration/E2E/Compra-Produto.ts
+++ cypress/integration/E2E/Compra-Produto.ts
@@ -1,22 +1,15 @@
 /// <reference types="cypress" />
-/// <reference types="cypress-xpath" />
-import produto from '../../support/pages/index'
-import realizar from '../../support/pages/checkout'
-describe('Acessar a tela de login e adiconar produtos ao carrinho carrinho com a finalidade de realizar o fluxo de compra.', () => { 
-
-    before('Before all', () => {
-        cy.visit('/')
-    })
-
-    it('Escolher um produto, suas especificações e adiconar ao carrinho', () => {
-        produto.comprar()
-        produto.abrirCarrinho()
-     });
-
-     it('Preencher o formulário com dados do usuário para realizar o pedido', () => {
-        realizar.checkout()
-        
-
-     })
-})
-
+import produto from "../../support/pages/index";
+import realizar from "../../support/pages/checkout";
+describe("Acessar a tela de login e adiconar produtos ao carrinho carrinho com a finalidade de realizar o fluxo de compra.", () => {
+  before("Before all", () => {
+    cy.visit("/");
+  });
+  it("Escolher um produto, suas especificações e adiconar ao carrinho", () => {
+    produto.comprar();
+    produto.abrirCarrinho();
+  });
+  it("Preencher o formulário com dados do usuário e realiza o pedido até o pagamento", () => {
+    realizar.checkout();
+  });
+});
